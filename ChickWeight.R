0917796671f0136e96790a27956dd8cb901e8212
陳瑞玉 <rita96030@gmail.com>
2018-01-02 19:54:02 +0800

    Signed-off-by: 陳瑞玉 <rita96030@gmail.com>

---
+++ ChickWeight.R
@@ -0,0 +1,23 @@
+a=1:10
+b=1:10
+c=11:20
+str(a)
+identical(a,b)
+identical(a,c)
+
+c=ChickWeight
+c[,2]
+x=c[,c("weight")]
+y=ChickWeight$weight 
+identical(x,y)
+
+y=ChickWeight[,c(4,1)] 
+identical(x,y)
+ChickWeight[,c(2:4)]
+
+m=ChickWeight[c(1,3,5,7,9),]
+nrow(c)
+n=c[ChickWeight$Chick == 20,] 
+
+
+
